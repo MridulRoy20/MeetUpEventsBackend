d1e9971dd63be5cb8fe56d1dc650690153c5b2b4
Mridul <mridul.mridulroy.roy@gmail.com>
2025-11-22 16:31:15 +0530

    Fix Vercel deployment

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ app.delete("/events/:id", async (req, res) => {
 
 // ----------------------
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
-
+// app.listen(PORT, () => {
+//   console.log("Server running on port", PORT);
+// });
+module.exports = app;
